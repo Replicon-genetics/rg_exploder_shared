657dfdaf280e405149797210b6bf17a1b83c6b96
Cazza <walklizard-replicon@yahoo.com>
2022-10-17 22:06:20 +0100

    Call_embl_feature_filter7_x.sh updates
    Not properly tested in rg_exploder-shared

--- helper_scripts/call_embl_feature_filter7_37.sh
+++ helper_scripts/call_embl_feature_filter7_37.sh
@@ -1,5 +1,6 @@
 # Shell script to list the Ensemble downloaded GB zipped files and run the filter program 
 root="/Users/caryodonnell/Documents/repositories/rg_exploder_shared/data_sources/"
+pythonroot="/Users/caryodonnell/Documents/repositories/rg_exploder_shared/helper_python"
 datadir="GRCH37_sequences_1000"
 jsonfile="_transcripts.json"
 ensembl="_ensembl"
@@ -19,7 +20,7 @@ locus=`echo $dir  | cut -f1 -d"_"`
 cd $pw/$1/$dir
 mv ensembl.txt.gz $locus$ensembl.gz
 gunzip $locus$ensembl.gz
-python3 /Users/caryodonnell/mytools/embl_feature_filter7.py -i $locus$ensembl -a $jopt -g $gopt
+python3 $pythonroot/embl_feature_filter7.py -i $locus$ensembl -a $jopt -g $gopt
 cp -p $locus$jsonfile ../../$datadir$curation/$locus$curation/.
 done
 # Now a fix for the KRAS / KRAS_minus setup
--- helper_scripts/call_embl_feature_filter7_38.sh
+++ helper_scripts/call_embl_feature_filter7_38.sh
@@ -1,6 +1,7 @@
 # Shell script to list the Ensemble downloaded GB zipped files and run the filter program 
+root="/Users/caryodonnell/Documents/repositories/rg_exploder_shared/data_sources/"
+pythonroot="/Users/caryodonnell/Documents/repositories/rg_exploder_shared/helper_python"
 datadir="GRCH38_sequences_1000"
-thispath="/Users/caryodonnell/Desktop/Replicon/"
 jsonfile="_transcripts.json"
 ensembl="_ensembl"
 curation="_curation"
@@ -8,10 +9,10 @@ dirtxt="_dir"
 # -j for longer json file with mRNA and CDS join data
 #jopt="-j"
 jopt=""
-# -gopt to correctly label release
+# -g to correctly label release
 gopt="38"
 
-cd $thispath$datadir
+cd $root$datadir
 pw=$PWD
 /bin/ls $1 | while read dir
 do
@@ -19,12 +20,19 @@ locus=`echo $dir  | cut -f1 -d"_"`
 cd $pw/$1/$dir
 mv ensembl.txt.gz $locus$ensembl.gz
 gunzip $locus$ensembl.gz
-#python3 /Users/caryodonnell/mytools/embl_feature_filter7.py -i $locus$ensembl -a
-
-python3 /Users/caryodonnell/mytools/embl_feature_filter7.py -i $locus$ensembl -a $jopt -g $gopt
+python3 $pythonroot/embl_feature_filter7.py -i $locus$ensembl -a $jopt -g $gopt
 cp -p $locus$jsonfile ../../$datadir$curation/$locus$curation/.
 done
 
+# Now a fix for the KRAS / KRAS_minus setup
+cd $root$datadir
+cp -p KRAS_dir/KRAS_transcripts.json ../$datadir$curation/KRAS_curation/.
+cp -p KRAS_minus_dir/KRAS_transcripts.json ../$datadir$curation/KRAS_minus_curation/.
+cd ../$datadir$curation/KRAS_minus_curation/
+sed -e 's/KRAS/KRAS_minus/g' KRAS_transcripts.json > KRAS_minus_transcripts.json
+/bin/rm KRAS_transcripts.json
+
+# Now a fix for the PTEN / PTEN_a setup 
 prelocus="PTEN"
 locus="PTEN_a"
 cd $locus$dirtxt
@@ -34,13 +42,5 @@ cp -p $prelocus$jsonfile ../../$datadir$curation/$locus$curation/$locus$jsonfile
 /bin/rm $prelocus$jsonfile--
 cd ..
 
-prelocus="KRAS"
-locus="KRAS_minus"
-cd $locus$dirtxt
-python3 /Users/caryodonnell/mytools/embl_feature_filter7.py -i $prelocus$ensembl -a $jopt -g $gopt
-sed -i -- 's/KRAS/KRAS_minus/g' $prelocus$jsonfile
-cp -p $prelocus$jsonfile ../../$datadir$curation/$locus$curation/$locus$jsonfile
-/bin/rm $prelocus$jsonfile--
-cd ..
-
 cd  $pw
+# Next step is: sh call_mash_json_38.sh
