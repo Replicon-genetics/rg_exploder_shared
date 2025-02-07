120759924079da81aed41430727d27d0c5bb65be
Cazza <walklizard-replicon@yahoo.com>
2025-02-07 12:02:30 +0000

    Update call_mash_json.sh

--- helper_scripts/call_mash_json.sh
+++ helper_scripts/call_mash_json.sh
@@ -5,28 +5,28 @@ rootdatadir=$rootapplicationdir"data_sources/"
 curation="_curation"
 app="helper_python/mash_json.py"
 
-input37="37"
-input38="38"
+GRCHver="stop"
+
+input37="GRCH37"
+input38="GRCH38"
 datadir37="GRCH37_sequences_1000"
-datadir38="GRCH37_sequences_1000"
+datadir38="GRCH38_sequences_1000"
+
+appropriate37=$rootdatadir$datadir37$curation
+appropriate38=$rootdatadir$datadir38$curation
+
+if echo "$PWD" | grep -q "$input37"; then GRCHver=input37 ; fi 
+if echo "$PWD" | grep -q "$input38"; then GRCHver=input38 ; fi 
 
 
-if [ "$1" == $input37 ]
+if [ $GRCHver != "stop" ]
 then
-    datadir=$input37
-elif [ "$1" == $input38 ]
-    then
-      datadir=$input38
+    echo "valid directory $PWD"
+    ##cd $rootdatadir$datadir$curation
+    python3 $rootapplicationdir$app
 else
-    datadir="stop"
-fi
-
-if [ $datadir != "stop" ]
-then
-    echo "valid parameter $1"
-    #cd $rootdatadir$datadir$curation
-    #python3 $rootapplicationdir$app
-else
-    echo "invalid parameter '$1': choose 37 or 38"
+    echo "$PWD is not an appropriate directory to run this."
+    echo " For $input37 cd to $appropriate37"
+    echo " For $input38 cd to $appropriate38"
 fi
 
