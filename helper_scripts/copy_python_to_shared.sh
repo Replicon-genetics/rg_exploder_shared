ed24037b0ae4c01b73cc21351c32755f615aabee
Cazza <walklizard-replicon@yahoo.com>
2025-02-18 21:06:12 +0000

    Copy scripts update
    Recoding and adding further documents compared to prior incarnation

--- helper_scripts/copy_python_to_shared.sh
+++ helper_scripts/copy_python_to_shared.sh
@@ -1,32 +1,43 @@
 # Copy latest versions across to shared python area
 rootdevdir="/Users/caryodonnell/Documents/repositories/snowlizardz/rg_exploder/"
-rootdevpythondir=$rootdevdir"exploder_python/"
-rootdevhelp_pythondir=$rootdevdir"helper_python/"
+devpythondir=$rootdevdir"exploder_python/"
+devhelp_pythondir=$rootdevdir"helper_python/"
 
 rootsharedir="/Users/caryodonnell/Documents/repositories/rg_exploder_shared/"
-rootsharepythondir=$rootsharedir"exploder_python/"
-rootsharehelp_pythondir=$rootsharedir"helper_python/"
+sharepythondir=$rootsharedir"exploder_python/"
+sharehelp_pythondir=$rootsharedir"helper_python/"
 
 same="/."
-cd $rootdevpythondir
-cp -p RG_exploder_globals.py $rootsharepythondir$same
-cp -p RG_exploder_globals_make.py $rootsharepythondir$same
-cp -p RG_exploder_gui.py $rootsharepythondir$same
-cp -p RG_exploder_io.py $rootsharepythondir$same
-cp -p RG_exploder_main.py $rootsharepythondir$same
-cp -p RG_exploder_process.py $rootsharepythondir$same
-cp -p RG_exploder_builder.py $rootsharepythondir$same
-cp -p Biopython_fix_metro.py $rootsharepythondir/Biopython_fix.py
+cd $devpythondir
+cp -p RG_exploder_globals.py $sharepythondir$same
+cp -p RG_exploder_globals_make.py $sharepythondir$same
+cp -p RG_exploder_gui.py $sharepythondir$same
+cp -p RG_exploder_io.py $sharepythondir$same
+cp -p RG_exploder_main.py $sharepythondir$same
+cp -p RG_exploder_process.py $sharepythondir$same
+cp -p RG_exploder_builder.py $sharepythondir$same
+cp -p Biopython_fix_metro.py $sharepythondir/Biopython_fix.py
 
-cd $rootdevhelp_pythondir
-cp -p embl_feature_filter8.py $rootsharehelp_pythondir$same
-cp -p embl_feature_filter_revise.py $rootsharehelp_pythondir$same
-cp -p mash_json.py $rootsharehelp_pythondir$same
-cp -p RG_exploder_io.py $rootsharehelp_pythondir$same
-cp -p gaussian_test.py $rootsharehelp_pythondir$same
-cp -p rayleigh_test.py $rootsharehelp_pythondir$same
-cp -p List_LRGs_GRCh38.txt $rootsharehelp_pythondir$same
-cp -p List_LRGs_GRCh37.txt $rootsharehelp_pythondir$same
+cd $devhelp_pythondir
+cp -p embl_feature_filter8.py $sharehelp_pythondir$same
+cp -p embl_feature_filter_revise.py $sharehelp_pythondir$same
+cp -p mash_json.py $sharehelp_pythondir$same
+cp -p RG_exploder_io.py $sharehelp_pythondir$same
+cp -p gaussian_test.py $sharehelp_pythondir$same
+cp -p rayleigh_test.py $sharehelp_pythondir$same
+cp -p List_LRGs_GRCh38.txt $sharehelp_pythondir$same
+cp -p List_LRGs_GRCh37.txt $sharehelp_pythondir$same
+cp -p config_setup.json $sharehelp_pythondir/config.json
+
+cd $sharehelp_pythondir
+ln -s ../exploder_python/RG_exploder_globals.py .
+ln -s ../exploder_python/RG_exploder_io.py .
+ln -s ../exploder_python/RG_exploder_process.py . 
+ln -s ../exploder_python/Biopython_fix.py .
+
+
+
+
 
 
 
--- helper_scripts/copy_scripts_to_shared.sh
+++ helper_scripts/copy_scripts_to_shared.sh
@@ -1,30 +1,72 @@
 # Copy latest versions across to shared scripts area
+
 rootdevdir="/Users/caryodonnell/Documents/repositories/snowlizardz/rg_exploder/"
-rootdevpythondir=$rootdevdir"exploder_python/"
-rootdevhelp_scriptdir=$rootdevdir"helper_scripts/"
+devhelp_scriptdir=$rootdevdir"helper_scripts/"
+devdocsdir=$rootdevdir"documents/"
+
+devAWSdocdir=$devdocsdir"AWS_data_management/"
+devnotesdir=$devdocsdir"development_journals/"
+
+devmaintdir=$devdocsdir"maintenance_guides/"
+devgenedatacurdir=$devmaintdir"Gene_data_curation/"
+devpresentationdir=$devdocsdir"presentations/"
+
+rootproddir="/Users/caryodonnell/Documents/repositories/mravn/rg_exploder/"
+prodpyodidedir=$rootproddir"pyodide/"
+prodwebdistdir=$rootproddir"webdist/"
 
 rootsharedir="/Users/caryodonnell/Documents/repositories/rg_exploder_shared/"
-rootsharepythondir=$rootsharedir"exploder_python/"
-rootsharehelp_scriptdir=$rootsharedir"helper_scripts/"
+sharehelp_scriptdir=$rootsharedir"helper_scripts/"
+sharedocsdir=$rootsharedir"documents/"
 
-same="/."
+shareAWSdocdir=$sharedocsdir"AWS_data_management/"
+sharenotesdir=$sharedocsdir"development_notes/"
 
-cd $rootdevhelp_scriptdir
-cp -p call_embl_feature_filter.sh $rootsharehelp_scriptdir$same
-cp -p call_mash_json.sh $rootsharehelp_scriptdir$same
+sharemaintdir=$sharedocsdir"maintenance_guides/"
+sharegenedatacurdir=$sharemaintdir"Gene_data_curation/"
 
-cp -p create_curation_tree.sh $rootsharehelp_scriptdir$same
-cp -p create_input_directory.sh $rootsharehelp_scriptdir$same
-cp -p create_output_directory.sh $rootsharehelp_scriptdir$same
+sharepyodidedir=$rootsharedir"pyodide/"
+sharewebdistdir=$rootsharedir"webdist/"
 
-cp -p switch_links.sh $rootsharehelp_scriptdir$same
-cp -p switch_Biopython_fix.sh $rootsharehelp_scriptdir$same
+same="."
 
-cp -p copy_python_to_shared.sh $rootsharehelp_scriptdir$same
-cp -p copy_scripts_to_shared.sh  $rootsharehelp_scriptdir$same
+cd $devhelp_scriptdir
+cp -p call_embl_feature_filter.sh $sharehelp_scriptdir$same
+cp -p call_mash_json.sh $sharehelp_scriptdir$same
+
+cp -p create_curation_tree.sh $sharehelp_scriptdir$same
+cp -p create_input_directory.sh $sharehelp_scriptdir$same
+cp -p create_output_directory.sh $sharehelp_scriptdir$same
+
+cp -p switch_links.sh $sharehelp_scriptdir$same
+cp -p switch_Biopython_fix.sh $sharehelp_scriptdir$same
+
+cp -p copy_python_to_shared.sh $sharehelp_scriptdir$same
+cp -p copy_scripts_to_shared.sh  $sharehelp_scriptdir$same
+
+cp -p make_rootRG.sh $sharehelp_scriptdir$same
+cp -p test_rootRG.sh $sharehelp_scriptdir$same
 
 
+cp -p $devAWSdocdir"AWS_deployments_register.ods"  $shareAWSdocdir$same
+cp -p $devnotesdir"exploder_issues_log.ods" $sharenotesdir$same
+cp -p $devgenedatacurdir"Replicon-specified_variations.ods" $sharegenedatacurdir$same
+cp -p $devmaintdir"Synthetic_Reads_Generator_webApp_updating."* $sharemaintdir$same
+cp -p $devmaintdir"Data_management."* $sharemaintdir$same
 
+cp -p $prodpyodidedir"copy_build_to_webdist.sh" $sharepyodidedir$same
+cp -p $prodpyodidedir"delete_old_build.sh" $sharepyodidedir$same
+cp -p $prodpyodidedir"tidy_dist.sh" $sharepyodidedir$same
+
+cp -p $prodpyodidedir"build/webworker.js" $sharepyodidedir"build/."
+cp -p $prodpyodidedir"packages/rg_exploder/rg_exploder/RG_exploder_io.py" $sharepyodidedir"packages/rg_exploder/rg_exploder/."
+cp -p $prodpyodidedir"packages/rg_exploder/rg_exploder/RG_exploder_webmain.py" $sharepyodidedir"packages/rg_exploder/rg_exploder/."
+cp -p $prodpyodidedir"packages/rg_exploder/rg_exploder/setup.py" $sharepyodidedir"packages/rg_exploder/rg_exploder/."
+
+
+cp -p $prodwebdistdir"public/webworker.js" $sharewebdistdir"public/."
+cp -p $prodwebdistdir"src/App.vue" $sharewebdistdir"src/."
+cp -p $prodwebdistdir"src/components/"* $sharewebdistdir"src/components/."
 
 
 
